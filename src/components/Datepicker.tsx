197835f9cadb729fb13ddc2fe034dd22c39e76ee
Andrey <stepgt@mail.ru>
2022-08-18 11:31:24 +1000

    Refactor code. Added utils

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -1,4 +1,13 @@
 import { useMemo, useState } from 'react';
+import {
+  getDaysAmountInAMonth,
+  getCurrentMonthDays,
+  getPreviousMonthDays,
+  getNextMonthDays,
+  DateCellItem,
+  dayOfWeek,
+  months,
+} from './utils';
 
 interface DatepickerProps {
   value: Date;
@@ -6,89 +15,6 @@ interface DatepickerProps {
   min?: Date;
   max?: Date;
 }
-
-interface DateCellItem {
-  date: number;
-  month: number;
-  year: number;
-  isToday?: boolean;
-  isSelected?: boolean;
-}
-
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-const dayOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-const getDaysAmountInAMonth = (year: number, month: number) => {
-  const nextMonthDate = new Date(year, month + 1, 1);
-  nextMonthDate.setMinutes(-1);
-  return nextMonthDate.getDate();
-};
-
-const sundayWeekToMondayWeekDayMap: Record<number, number> = {
-  0: 6,
-  1: 0,
-  2: 1,
-  3: 2,
-  4: 3,
-  5: 4,
-  6: 5
-}
-
-const getDayOfWeek = (date: Date) => {
-  const day = date.getDay();
-  return sundayWeekToMondayWeekDayMap[day];
-}
-
-const getPreviousMonthDays = (year: number, month: number) => {
-  const currentMonthFirstDay = new Date(year, month, 1);
-  const prevMonthCellsAmount = getDayOfWeek(currentMonthFirstDay);
-  //
-  const daysAmountInPrevMonth = getDaysAmountInAMonth(year, month - 1);
-  //
-  const dateCells: DateCellItem[] = [];
-  const [cellYear, cellMonth] = month === 0 ? [year - 1, 11] : [year, month - 1];
-  for (let i = prevMonthCellsAmount - 1; i >= 0; i--) {
-    dateCells.push({
-      year: cellYear,
-      month: cellMonth,
-      date: daysAmountInPrevMonth - i,
-    });
-  }
-  return dateCells;
-};
-
-const VISIBLE_CELLS_AMOUNT = 6 * 7;
-
-const getNextMonthDays = (year: number, month: number) => {
-  const currentMonthFirstDay = new Date(year, month, 1);
-  const prevMonthCellsAmount = getDayOfWeek(currentMonthFirstDay);
-  const daysAmount = getDaysAmountInAMonth(year, month);
-  const nextMonthDays = VISIBLE_CELLS_AMOUNT - daysAmount - prevMonthCellsAmount;
-  //
-  const dateCells: DateCellItem[] = [];
-  const [cellYear, cellMonth] = month === 11 ? [year + 1, 0] : [year, month + 1];
-  for (let i = 1; i <= nextMonthDays; i++) {
-    dateCells.push({
-      year: cellYear,
-      month: cellMonth,
-      date: i,
-    });
-  }
-  return dateCells;
-};
-
-const getCurrentMonthDays = (year: number, month: number, numberOfDays: number) => {
-  const dateCells: DateCellItem[] = [];
-  for (let i = 1; i <= numberOfDays; i++) {
-    dateCells.push({
-      year,
-      month,
-      date: i,
-    });
-  }
-  return dateCells;
-};
 
 const Datepicker = ({ value, onChange, min, max }: DatepickerProps) => {
   const [panelYear, setPanelYear] = useState(() => value.getFullYear());
